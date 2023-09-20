946972cca99a40962b97935b4553860e40f55f49
Nick Wang <nick.jxw@gmail.com>
2023-09-20 01:06:40 -0400

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,52 @@
+export default function About() {
+    return (
+        <div className='max-w-6xl mx-auto space-y-4'>
+            <h1 className='text-2xl font-medium text-amber-500'>About</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                deserunt, magni velit modi laudantium, veritatis qui aliquid,
+                suscipit omnis culpa rem? Maxime debitis illum sed porro
+                doloribus magnam possimus reiciendis odio itaque praesentium qui
+                accusantium consectetur similique aperiam in veniam, libero
+                veritatis? Delectus maxime corporis maiores, blanditiis totam
+                debitis voluptas voluptates aliquid iure nulla repellat
+                quibusdam similique molestiae laboriosam dignissimos id? Beatae
+                ipsa dolorum laudantium architecto eius nesciunt possimus
+                excepturi provident, expedita nihil molestiae optio dolore quas
+                quasi eaque eos fugit error? Dicta ipsa molestias perferendis
+                quas dolorem necessitatibus quos sapiente? Hic accusamus
+                voluptas praesentium minus id voluptates pariatur perferendis.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Suscipit commodi illum temporibus aliquid error autem voluptate?
+                Nesciunt minima sunt id delectus similique corporis ex?
+                Reprehenderit itaque rem deleniti, repudiandae provident
+                aspernatur veritatis facere tempora delectus ipsam eaque. Nisi
+                neque rem sequi, dolorem velit odio possimus exercitationem
+                doloremque ipsum repudiandae? Est, quisquam maxime. At nam modi
+                quos est ducimus! Fuga vel corrupti doloremque, tenetur quaerat
+                provident fugit incidunt amet pariatur ipsa aut expedita neque.
+                Nesciunt repudiandae autem est commodi explicabo voluptate
+                doloribus, vel eaque, quis obcaecati laudantium dolorem sed
+                placeat. Quae voluptatum ratione qui iste provident, ab quia
+                minus itaque ipsa.
+            </p>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod
+                magnam, blanditiis nisi sed dignissimos, sunt, voluptates ab
+                dicta asperiores deserunt suscipit neque veritatis tenetur dolor
+                iure ut accusantium delectus qui voluptas debitis provident
+                aspernatur. Id vel voluptatem dolorum sunt laborum ipsum
+                voluptatibus commodi aliquam tenetur vitae esse quidem ab error
+                ea numquam, aspernatur harum! Perferendis rem hic fuga ipsum
+                consequatur, dolores, ratione maxime ea explicabo distinctio
+                minus quam impedit nostrum sunt magnam? Tenetur consectetur
+                commodi illo ipsa quam quia ullam molestiae dolores distinctio.
+                Quam doloribus nostrum aspernatur ullam reprehenderit itaque, ea
+                nemo illum accusamus tempora unde voluptatum ipsum perferendis
+                quas.
+            </p>
+        </div>
+    )
+}
